e6eccd8f172ff1d7729741795acd61b343957c86
IsaacPonce13 <111556869+IsaacPonce13@users.noreply.github.com>
2022-08-24 01:39:12 -0500

    Add files via upload

---
+++ MenuPyC.js
@@ -0,0 +1,25 @@
+function SelectProd() {
+    document.getElementById("Productos").style.display = "block"
+    document.getElementById("Menu").style.display = "none"
+}
+            //Mostrar formato clientes
+function SelectClie() {
+    document.getElementById("Clientes").style.display = "block"
+    document.getElementById("Menu").style.display = "none"
+}
+function RegresarMenu() {
+    document.getElementById("Productos").style.display = "none"
+    document.getElementById("Clientes").style.display = "none"
+    document.getElementById("Menu").style.display = "flex"
+}
+    //ocultar elementos
+    //Clientes
+    document.getElementById("guardar").style.display = "none"
+    document.getElementById("cancelar").style.display = "none"
+    //Productos
+    document.getElementById("guardarP").style.display = "none"
+    document.getElementById("cancelarP").style.display = "none"
+    //Formularios
+    document.getElementById("Clientes").style.display = "none"
+    document.getElementById("Productos").style.display = "none"
+    //Fin ocultar elementos.
