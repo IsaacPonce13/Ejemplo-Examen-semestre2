54b855f3a81e2605ee9398574093075a92b2d311
IsaacPonce13 <111556869+IsaacPonce13@users.noreply.github.com>
2022-11-20 21:55:28 -0600

    plantilla login y js innecesarios
    Esta version tiene una plantilla para el login y le quite los archivos que hacen funcionar el proyecto con local storage

--- MenuPyC.js
+++ MenuPyC.js
@@ -1,12 +1,15 @@
+//Mostrar formulario productos
 function SelectProd() {
     document.getElementById("Productos").style.display = "block"
     document.getElementById("Menu").style.display = "none"
 }
-            //Mostrar formato clientes
+
+//Mostrar formulario clientes
 function SelectClie() {
     document.getElementById("Clientes").style.display = "block"
     document.getElementById("Menu").style.display = "none"
 }
+//Funcion del boton regresar al menu
 function RegresarMenu() {
     document.getElementById("Productos").style.display = "none"
     document.getElementById("Clientes").style.display = "none"
